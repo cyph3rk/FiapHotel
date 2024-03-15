9f85fa36954ba7682b74987a6fadd2ea711ac93f
Diego Vargas <diego.vargas@digitro.com>
2024-03-15 11:45:41 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# FiapHotel
+Hackathon Totalmente desnecessário no meio do projeto da 5a fase Pos Tech Fiap
---
+++ src/main/java/br/com/fiap/fiaphotel/FiapHotelApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.fiaphotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiapHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiapHotelApplication.class, args);
+	}
+
+}
